387a509f36519c9e4647552040f6079b7cb954ca
Martin Dzhonov <mdzhonov@gls-bg.net>
2020-01-14 16:02:19 +0200

    config fixes

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
-const path = require('path');
 const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
-const publicPath = path.join(__dirname, '..', 'public');
-const port = process.env.PORT || 3000;
-app.use(express.static(publicPath));
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname + '/dist'));
+
+// send the user to index html page inspite of the url
 app.get('*', (req, res) => {
-   res.sendFile(path.join(publicPath, 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
-app.listen(port, () => {
-   console.log('Server is up!');
-});
+
+app.listen(port);
